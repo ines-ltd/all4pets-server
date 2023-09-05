7188db0b45e9411778fc00442fc865d7816ce0b4
nimonian <nimon@hotmail.co.uk>
2023-09-05 23:52:59 +0100

    adds s3 fileservice

--- medusa-config.js
+++ medusa-config.js
@@ -1,37 +1,37 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv')
 
-let ENV_FILE_NAME = "";
+let ENV_FILE_NAME = ''
 switch (process.env.NODE_ENV) {
-  case "production":
-    ENV_FILE_NAME = ".env.production";
-    break;
-  case "staging":
-    ENV_FILE_NAME = ".env.staging";
-    break;
-  case "test":
-    ENV_FILE_NAME = ".env.test";
-    break;
-  case "development":
+  case 'production':
+    ENV_FILE_NAME = '.env.production'
+    break
+  case 'staging':
+    ENV_FILE_NAME = '.env.staging'
+    break
+  case 'test':
+    ENV_FILE_NAME = '.env.test'
+    break
+  case 'development':
   default:
-    ENV_FILE_NAME = ".env";
-    break;
+    ENV_FILE_NAME = '.env'
+    break
 }
 
 try {
-  dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
+  dotenv.config({ path: process.cwd() + '/' + ENV_FILE_NAME })
 } catch (e) {}
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
-  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+  process.env.ADMIN_CORS || 'http://localhost:7000,http://localhost:7001'
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
+const STORE_CORS = process.env.STORE_CORS || 'http://localhost:8000'
 
 const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+  process.env.DATABASE_URL || 'postgres://localhost/medusa-store'
 
-const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379'
 
 const plugins = [
   `medusa-fulfillment-manual`,
@@ -39,20 +39,32 @@ const plugins = [
   {
     resolve: `@medusajs/file-local`,
     options: {
-      upload_dir: "uploads",
-    },
+      upload_dir: 'uploads'
+    }
   },
   {
-    resolve: "@medusajs/admin",
+    resolve: '@medusajs/admin',
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
       autoRebuild: false,
       develop: {
-        open: process.env.OPEN_BROWSER !== "false",
-      },
-    },
+        open: process.env.OPEN_BROWSER !== 'false'
+      }
+    }
   },
-];
+  {
+    resolve: `medusa-file-s3`,
+    options: {
+      s3_url: process.env.S3_URL,
+      bucket: process.env.S3_BUCKET,
+      region: process.env.S3_REGION,
+      access_key_id: process.env.S3_ACCESS_KEY_ID,
+      secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+      cache_control: process.env.S3_CACHE_CONTROL,
+      download_file_duration: process.env.S3_DOWNLOAD_FILE_DURATION
+    }
+  }
+]
 
 const modules = {
   /*eventBus: {
@@ -67,7 +79,7 @@ const modules = {
       redisUrl: REDIS_URL
     }
   },*/
-};
+}
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
 const projectConfig = {
@@ -78,11 +90,11 @@ const projectConfig = {
   admin_cors: ADMIN_CORS,
   // Uncomment the following lines to enable REDIS
   redis_url: REDIS_URL
-};
+}
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
 module.exports = {
   projectConfig,
   plugins,
-  modules,
-};
+  modules
+}
